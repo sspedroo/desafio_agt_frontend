52d890e747d563976c88583310b424ec0c6dc9c7
Pedro Oliveira <012pedropaulo@gmail.com>
2025-07-18 03:51:11 -0300

    Add FormularioRegistrarRetorno component with HTML structure, SCSS styles, and TypeScript logic for registering returns

---
+++ src/app/components/formulario-registrar-retorno/formulario-registrar-retorno.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-registrar-retorno',
+  imports: [],
+  templateUrl: './formulario-registrar-retorno.component.html',
+  styleUrl: './formulario-registrar-retorno.component.scss'
+})
+export class FormularioRegistrarRetornoComponent {
+
+}
