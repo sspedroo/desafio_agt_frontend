8fc0809277592f00edcffd464e89ec40a63675b7
Pedro Oliveira <012pedropaulo@gmail.com>
2025-07-19 14:28:58 -0300

    feat: criado page para realizar acoes crud de funcionarios

---
+++ src/app/pages/funcionarios/funcionarios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionarios',
+  imports: [],
+  templateUrl: './funcionarios.component.html',
+  styleUrl: './funcionarios.component.scss'
+})
+export class FuncionariosComponent {
+
+}
